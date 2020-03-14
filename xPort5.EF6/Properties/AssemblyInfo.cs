8dc7dc68688cd2260768b8aef09948e2a165e619
Paulus Yeung <paulusyeung@nxstudio.com>
2020-03-14 13:29:03 +0800

    revised compyright notice and updated packages

--- xPort5.EF6/Properties/AssemblyInfo.cs
+++ xPort5.EF6/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("xPort5.EF6")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2020 nxStudio")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
